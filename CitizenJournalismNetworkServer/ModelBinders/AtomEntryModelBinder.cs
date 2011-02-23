7e61f64195fe54079384b9679af25024f4701c1b
Edgar Verona <EdgarVerona@gmail.com>
2011-02-22 20:38:09 -0500

    Day 18
    Yeah, I did a lot.  NOT.
    
    Faulty internet connection + forgetting to pull the latest version = wasted morning.

---
+++ CitizenJournalismNetworkServer/ModelBinders/AtomEntryModelBinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CitizenJournalismNetworkServer.ModelBinders
+{
+
+    public class AtomEntryModelBinder: DefaultModelBinder
+    {
+
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            return base.BindModel(controllerContext, bindingContext);
+        }
+
+    }
+
+}
