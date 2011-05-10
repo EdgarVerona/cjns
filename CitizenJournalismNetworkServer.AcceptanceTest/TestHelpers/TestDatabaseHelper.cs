8a8e0f4ad94b9d388a2fd264f5a179874e544460
Edgar Verona <EdgarVerona@gmail.com>
2011-05-09 21:23:42 -0400

    Adding notes on Acceptance Tests

--- CitizenJournalismNetworkServer.AcceptanceTest/TestHelpers/TestDatabaseHelper.cs
+++ CitizenJournalismNetworkServer.AcceptanceTest/TestHelpers/TestDatabaseHelper.cs
@@ -17,9 +17,78 @@ namespace CitizenJournalismNetworkServer.AcceptanceTest.TestHelpers
 
         public static void ClearDatabase()
         {
-            /*SqlConnection conn = new SqlConnection("TEST");
+            SqlConnection conn = new SqlConnection("TEST");
             SqlCommand command = new SqlCommand();
-            command.CommandText = "DELETE FROM ";*/
+
+            StringBuilder sql = new StringBuilder();
+
+            sql.Append("DELETE FROM EntryCategories;");
+            sql.Append("DELETE FROM CollectionCategories;");
+            sql.Append("DELETE FROM CollectionContentTypes;");
+            sql.Append("DELETE FROM WorkspaceCollections;");
+            sql.Append("DELETE FROM EntryAuthors;");
+            sql.Append("DELETE FROM EntryContributors;");
+            sql.Append("DELETE FROM Links;");
+            sql.Append("DELETE FROM Categories;");
+            sql.Append("DELETE FROM ContentTypes");
+            sql.Append("DELETE FROM Workspaces;");
+            sql.Append("DELETE FROM People;");
+            sql.Append("DELETE FROM Entries;");
+            sql.Append("DELETE FROM Collections;");
+
+            command.CommandText = sql.ToString();
+            command.CommandType = System.Data.CommandType.Text;
+
+            command.ExecuteNonQuery();
+        }
+
+        public static IList<ContentType> CreateTestContentTypes()
+        {
+            List<ContentType> results = new List<ContentType>();
+
+            ContentTypeRepository repo = new ContentTypeRepository();
+
+            ContentType item = new ContentType()
+            {
+                Text = "text"
+            };
+
+            results.Add(item);
+            repo.Add(item);
+
+            item = new ContentType()
+            {
+                Text = "html"
+            };
+
+            results.Add(item);
+            repo.Add(item);
+
+            item = new ContentType()
+            {
+                Text = "xhtml"
+            };
+
+            results.Add(item);
+            repo.Add(item);
+
+            repo.Save();
+
+            return results;
+        }
+
+
+        public static Collection CreateTestCollection(string title)
+        {
+            Collection result = new Collection()
+            {
+                AreCategoriesFixed = true,
+                AtomId = Guid.NewGuid().ToString(),
+                DateCreated = DateTime.UtcNow,
+                Title = title
+            };
+
+            return result;
         }
 
     }
